af40d67dc2e41890a927c6ccfcc833203ffcd785
Aman Sharma <124869433+amansharma253@users.noreply.github.com>
2024-09-06 10:43:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find all factorial numbers less than or equal to n/find-all-factorial-numbers-less-than-or-equal-to-n.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<long long> factorialNumbers(long long n) {
+        // Write Your Code here
+        long long fact=1,i=1;
+        vector<long long> v;
+        
+        
+        
+        while(fact<=n)
+        {
+            v.push_back(fact);
+            i++;
+            fact*=i;
+        }
+        return v;
+    }       
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+        vector<long long> ans = ob.factorialNumbers(N);
+        for (auto num : ans) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
