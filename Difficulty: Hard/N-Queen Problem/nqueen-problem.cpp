f2257ac0e4f428e3a27dc02ef4e1d0e5462a46f9
Aman Sharma <124869433+amansharma253@users.noreply.github.com>
2025-01-30 22:07:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -4,101 +4,74 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-     vector<vector<int>> ans;
-    
-    bool check(vector<vector<char>>& board , int i , int j){
-        int x = i , y = j;
-        // up
-        while(x >= 0){
-            if(board[x][y] == 'Q'){
-                return false;
-            }
-            x--;
-        }
-        // left diag up
-        x = i;
-        while(x >= 0 && y >= 0){
-            if(board[x][y] == 'Q') return false;
-            x--;
-            y--;
-        }
-        // right diag up
-        x = i , y = j;
-        while(x >= 0 && y < board.size()){
-            if(board[x][y] == 'Q') return false;
-            x--;
-            y++;
-        }
-        return true;
-    }
-    
-    void insertAns(vector<vector<char>>& board){
-        vector<int> row;
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board.size();j++){
-                if(board[j][i] == 'Q'){
-                    row.push_back(j+1);
-                    break;
-                }
-            }
-        }
-        if(row.size() == board.size()){
-            ans.push_back(row);
-        }
-    }
+class Solution {
+  public:
+    vector<vector<int>> res;
 
-
-    void solve(vector<vector<char>> &board , int i , int n){
-        if(i >= n){
-            insertAns(board);
+    void solve(int col, int n, vector<vector<int>> &board, vector<int> &temp, vector<int> &leftRow, vector<int> &lowDiag, vector<int> &upDiag)
+    {
+        if(col==n){
+            res.push_back(temp);
             return;
         }
-        
-        for(int j=0;j<board.size();j++){
-            if(check(board,i,j)){
-                board[i][j] = 'Q';
-                solve(board,i+1,n);
-                board[i][j] = '-';
+        for(int row=0;row<n;row++){
+            if(leftRow[row]==0 && lowDiag[row+col]==0 && upDiag[n-1+col-row]==0){
+                board[row][col]=1;
+                leftRow[row]=1;
+                lowDiag[row+col]=1;
+                upDiag[n-1+col-row]=1;
+                temp.push_back(row+1);
+                
+                solve(col+1,n,board,temp,leftRow,lowDiag,upDiag);
+                
+                temp.pop_back();
+                board[row][col]=0;
+                leftRow[row]=0;
+                lowDiag[row+col]=0;
+                upDiag[n-1+col-row]=0;
             }
         }
-        
     }
-
     vector<vector<int>> nQueen(int n) {
-        vector<vector<char>> board(n,vector<char>(n,'-'));
-        solve(board,0,n);
-        sort(ans.begin(),ans.end());
-        return ans;
+        // code here
+        vector<vector<int>> board(n,vector<int>(n));
+        vector<int> temp;
+        vector<int> leftRow(n), lowDiag(2*n-1), upDiag(2*n-1);
+        solve(0,n,board,temp,leftRow,lowDiag,upDiag);
+        return res;
     }
 };
 
 //{ Driver Code Starts.
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        
+        cin >> n;
+
         Solution ob;
         vector<vector<int>> ans = ob.nQueen(n);
-        if(ans.size() == 0)
-            cout<<-1<<"\n";
+        if (ans.size() == 0)
+            cout << -1 << "\n";
         else {
-            for(int i = 0;i < ans.size();i++){
-                cout<<"[";
-                for(int u: ans[i])
-                    cout<<u<<" ";
-                cout<<"] ";
+            sort(ans.begin(), ans.end());
+            for (int i = 0; i < ans.size(); i++) {
+                cout << "[";
+                for (int u : ans[i])
+                    cout << u << " ";
+                cout << "] ";
             }
-            cout<<endl;
+            cout << endl;
         }
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
