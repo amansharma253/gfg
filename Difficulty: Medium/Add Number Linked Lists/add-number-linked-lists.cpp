3238a420c5c677cef8b763bc7b5fd4019968fcff
Aman Sharma <124869433+amansharma253@users.noreply.github.com>
2025-12-30 13:25:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.cpp
+++ Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.cpp
@@ -1,170 +1,62 @@
-//{ Driver Code Starts
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-using namespace std;
 
-/* Linked list Node */
-struct Node {
-    int data;
-    struct Node* next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-Node* buildList() {
-    vector<int> arr;
-    string input;
-    getline(cin, input);
-    stringstream ss(input);
-    int number;
-    while (ss >> number) {
-        arr.push_back(number);
-    }
-    if (arr.empty()) {
-        return NULL;
-    }
-    int val = arr[0];
-    int size = arr.size();
-
-    Node* head = new Node(val);
-    Node* tail = head;
-
-    for (int i = 1; i < size; i++) {
-        val = arr[i];
-        tail->next = new Node(val);
-        tail = tail->next;
-    }
-
-    return head;
-}
-
-void printList(Node* n) {
-    while (n) {
-        cout << n->data << " ";
-        n = n->next;
-    }
-    cout << endl;
-}
-
-
-// } Driver Code Ends
-/* node for linked list:
-
-struct Node {
-    int data;
-    struct Node* next;
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-*/
 
 class Solution {
   public:
-  Node* reverse(Node*head){
-      Node*prev=NULL;
-      Node*next;
-      Node*curr=head;
-      while(curr!=NULL){
-          next=curr->next;
-          curr->next=prev;
-          prev=curr;
-          curr=next;
-      }
-      return prev;
-  }
-    Node* addTwoLists(Node* num1, Node* num2) {
-        // code here
-        while(num1->data==0){
-            num1=num1->next;
+    pair<Node*,int> reverse(Node *head){
+        Node* curr=head,*prev=NULL;
+        int len=0;
+        while(curr!=NULL){
+            Node *next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            len++;
         }
-         while(num2->data==0){
-            num2=num2->next;
-        }
-        
-        Node*n1=reverse(num1);
-        Node*n2=reverse(num2);
-        Node*temp=new Node(-1);
-        Node*sum=temp;
+        return {prev,len};
+    }
+    void add(Node* head1, Node* head2){  //add head2 into head1 where len(head1)>=len(head2)
+        Node* curr1=head1,*curr2=head2;
         int carry=0;
-        while(n1!=NULL&&n2!=NULL){
-            int a=n1->data;
-            int b=n2->data;
-            int digit=a+b+carry;
-            carry=digit/10;
-           int value=digit%10;
-            Node*newNode=new Node(value);
-            temp->next=newNode;
-            temp=newNode;
-            n1=n1->next;
-            n2=n2->next;
-        }
-        while(n1!=NULL){
-            int a=n1->data;
-            int digit=a+carry;
-             carry=digit/10;
-          int  value=digit%10;
-           
-             Node*newNode=new Node(value);  
-               temp->next=newNode;
-               temp=newNode;
-            
-            n1=n1->next;
-        }
-            
-        
-        while(n2!=NULL){
-            int b=n2->data;
-            int digit=b+carry;
-             carry=digit/10;
-           int value=digit%10;
-           
-             Node*newNode=new Node(value);  
-             temp->next=newNode;
-            temp=newNode;
-            n2=n2->next;
-           
-            
-        }
-        while(carry!=0){
-            int digit=carry%10;
-            if(digit==0){
-                break;
+        while(curr1!=NULL){
+            int add=carry;
+            if(curr2!=NULL)
+                add+=curr2->data;
+            curr1->data+=add;
+            carry=0;
+            if(curr1->data>=10){
+                carry=1;
+                curr1->data-=10;
             }
-            carry=carry/10;
-            Node*newNode=new Node(digit);
-            temp->next=newNode;
-            temp=newNode;
+            if(curr1->next==NULL and carry)
+                curr1->next=new Node(0);
+            curr1=curr1->next;
+            if(curr2!=NULL)
+                curr2=curr2->next;
         }
-        Node*ans=reverse(sum->next);
-        return ans;
+    } 
+    Node* remove0(Node *head){
+        Node* curr=head;
+        while(curr!=NULL and curr->data==0)
+            curr=curr->next;
+        return curr;
+    }
+    void print(Node* head){
+        Node* curr=head;
+        while(curr!=NULL){
+            cout<<curr->data<<" -> ";
+            curr=curr->next;
+        }
+        cout<<endl;
+    }
+    Node* addTwoLists(Node* head1, Node* head2) {
+        auto [rhead1,len1]=reverse(head1);
+        auto [rhead2,len2]=reverse(head2);
+        if(len2>len1)
+            swap(rhead1,rhead2);
+        // print(rhead1);
+        // print(rhead2);
+        add(rhead1,rhead2);
+        auto [ans,ans_len]=reverse(rhead1);
+        return remove0(ans);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // To ignore the newline character after the integer input
-
-    while (t--) {
-        Node* num1 = buildList();
-        Node* num2 = buildList();
-        Solution ob;
-        Node* res = ob.addTwoLists(num1, num2);
-        printList(res);
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
